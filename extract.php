e53ac5160956d5cbc1d570953975cc5e8fe84dc1
Lukas Rademacher <lukas@rademacher.ac>
2016-12-15 11:56:54 +0100

    initial commit

---
+++ extract.php
@@ -0,0 +1,54 @@
+<?php
+
+if(count($argv) < 2){
+    echo 
+    "usage: php extract.php path\n".
+    "example: php scan.php /tmp/php-chunked-xhtml/\n".
+    "\n";
+    exit(1);
+}
+
+
+/* Directory with extracted php-docs
+ * from http://php.net/download-docs.php (Many HTML files tar.gz)
+ */
+
+$dir_name = $argv[1];
+$deprecated_functions = array();
+if ($handle = opendir($dir_name)) {
+    while (false !== ($entry = readdir($handle))) {
+        $file_content = "";
+        $deprecated = array();
+        $removed = array();
+        if (!is_dir($entry) && (preg_match('/function.*php$/',$entry) !== false)) {
+			$file_content = file_get_contents($dir_name.$entry);
+            $file_content = str_replace("\n", "", $file_content);
+            $file_content = str_replace("\r", "", $file_content);
+            $deprecated = array();
+            if(preg_match("/Warning\s*This\s*(function|extension)\s*(was|has been)\s*DEPRECATED\s*(in|as of) PHP ([0-9].[0-9].[0-9])/i", strip_tags($file_content),$deprecated) !== false){
+                 $function = array();
+                 if(preg_match('/<h1 class="refname">([a-zA-Z:_-]*)<\/h1>/', $file_content,$function) !== false){
+                    if(!isset($function[1])){
+                        continue;
+                    }
+                 }
+                 //print_r($deprecated);
+                if(isset($deprecated[4])){
+                    $deprecated_functions[$function[1]]['deprecated'] = $deprecated[4];
+                }
+                $removed = array();
+                if(preg_match("/Warning\s*This\s*(function|extension).*REMOVED\s*(in|as of) PHP\s+([0-9].[0-9].[0-9])/i", strip_tags($file_content),$removed) !== false){
+                    if(isset($removed[3])){
+                        $deprecated_functions[$function[1]]['removed'] = $removed[3];
+                    }
+                }
+            }
+        }
+        unset($file_content);
+        unset($deprecated);
+        unset($removed);
+    }
+    closedir($handle);
+}
+print_r($deprecated_functions);
+file_put_contents(dirname(__FILE__).DIRECTORY_SEPARATOR."data.json", json_encode($deprecated_functions));
---
+++ scan.php
@@ -0,0 +1,69 @@
+<?php
+if(count($argv) < 3){
+	echo 
+	"usage: php scan.php version path\n".
+	"example: php scan.php 7.0.0 /var/www/project\n".
+	"\n";
+	exit(1);
+}
+
+if(!preg_match("/[0-9]\.[0-9]\.[0-9]/", $argv[1])){
+	echo "Version number ".$argv[1]." does not match [0-9].[0-9].[0-9]\n";
+	exit(1);
+}
+
+if(!is_dir($argv[2])){
+	echo "Directory ".$argv[2]." not found\n";
+	exit(1);
+}
+
+$datafile = dirname(__FILE__).DIRECTORY_SEPARATOR."data.json";
+if(isset($argv[3])){
+	$datafile = $argv[3];
+}
+
+
+if(!file_exists($datafile)){
+	echo "Datafile ".$datafile." not found, please execute extract first.\n";
+	exit(1);
+}
+
+$dir_name = $argv[2];
+$check_version = $argv[1];
+
+$count = array();
+$deprecated_functions = file_get_contents($datafile);
+$deprecated_functions = json_decode($deprecated_functions);
+$count = checkdir($dir_name,$deprecated_functions,$check_version);
+
+print_r($count);
+
+function checkdir($dir_name,$deprecated_functions,$check_version,$count = array()) {
+   $cdir = scandir($dir_name);
+   foreach ($cdir as $key => $value){
+      if (!in_array($value,array(".",".."))){
+         if (is_dir($dir_name . DIRECTORY_SEPARATOR . $value)){
+             $count = checkdir($dir_name . DIRECTORY_SEPARATOR . $value,$deprecated_functions,$check_version,$count);
+         }
+         elseif(preg_match('/php$/',$value) ==! false){
+         	$file_contents = file_get_contents($dir_name . DIRECTORY_SEPARATOR . $value);
+         	foreach($deprecated_functions as $function_name => $depend){
+         		$function_name = preg_replace("/^.*::/",'', $function_name);
+         		if(preg_match('/\b'.$function_name.'\s*\(/', $file_contents)){
+         			if(isset($count[$function_name])){
+         				$count[$function_name] ++;
+         			}else{
+         				$count[$function_name] = 1;
+         			}
+         			if (version_compare($depend->removed, $check_version,'<=')) {
+         				echo "removed in PHP ".$depend->removed.": ".$function_name." \t\tin ".$dir_name . DIRECTORY_SEPARATOR . $value."\n" ;
+         			}elseif (version_compare($depend->deprecated, $check_version,'<=')) {
+         				echo "deprecated in PHP ".$depend->deprecated.": ".$function_name." \t\tin ".$dir_name . DIRECTORY_SEPARATOR . $value."\n" ;
+         			}
+         		}
+         	}
+         }
+      }
+   }
+   return $count;
+}
